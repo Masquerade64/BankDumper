5c9b17cc1a26dae3abd846c20f351274a5e1b7a0
Masquerade <87361986+Masquerade64@users.noreply.github.com>
2022-03-09 19:32:35 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace BankDumper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine(" " + AppDomain.CurrentDomain.FriendlyName + " <input bank> <output fsb>");
+            }
+            else if (args.Length == 1)
+            {
+                Console.WriteLine();
+                Console.WriteLine(" " + AppDomain.CurrentDomain.FriendlyName + " <input bank> <output fsb>");
+            }
+            else if (args.Length == 2)
+            {
+                byte[] infile = File.ReadAllBytes(args[0]);
+
+                for (int i = 0; i < infile.Length; i++)
+                {
+                    bool found = false;
+                    string buffer = infile[i].ToString() + infile[i + 1].ToString() + infile[i + 2].ToString() + infile[i + 3].ToString();
+                    if (buffer == "70836653")
+                    {
+                        int newlen = infile.Length - i;
+                        byte[] outfile = new byte[newlen];
+                        Array.Copy(infile, i, outfile, 0, newlen);
+                        File.WriteAllBytes(args[1], outfile);
+                        found = true;
+                    }
+                    if (found == true)
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine(" " + AppDomain.CurrentDomain.FriendlyName + " <input bank> <output fsb>");
+            }
+        }
+    }
+}
